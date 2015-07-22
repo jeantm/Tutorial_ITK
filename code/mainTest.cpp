cbea844f09ab71e671e0c4bf0bcc39021ee6952b
Jean <jeantm@email.unc.edu>
2015-07-22 17:03:53 -0400

    ITK Tutorial Create and manage simple images and filters 4th commit Added : 	- mainTest.xml Correction : 	- small corrections in CMakeLists.txt and mainTest.cpp
    Project compiles and runs

--- code/mainTest.cpp
+++ code/mainTest.cpp
@@ -1,36 +1,40 @@
+#include "itkImage.h"
 #include "itkImageFileReader.h"
-#include "itkImageFileWriter.h"
 #include "itkThresholdImageFilter.h"
 
 
 
-typedef unsigned char PixelType;
-const unsigned int Dimension = 2;
-
-typedef itk::Image< PixelType, Dimension > ImageType;
-
-typedef itk::ThresholdImageFilter< ImageType > FilterType;
-
-typedef itk::ImageFileReader< ImageType > ReaderType;
-typedef itk::ImageFileWriter< ImageType > WriterType;
 
 int main(int argc, char ** argv)
 {
-  if (argc != 1)
-  {
-    printf("Wrong Number of inputs for this main./n Please enter only one input image./n")
-  }
-  else
-  {
-    ReaderType::Pointer reader = ReaderType::New();
-    FilterType::Pointer filter = FilterType::New();
+    typedef unsigned char PixelType;
+    const unsigned int Dimension = 2;
+    typedef itk::Image< PixelType, Dimension > ImageType;    
+    
+    ImageType::Pointer image;
 
-    reader->SetFileName( argv[1] );
+    if (argc < 2)
+    {
+        printf("Wrong Number of inputs for this main./n Please enter only one input image./n");
+        return EXIT_FAILURE;
+    }
 
-    filter->SetInput( reader->GetOutput() );
-    filter->SetOutsideValue( 0 );
-    filter->ThresholdOutside( 100, 170 );
-    filter->Update();
-  }
+    else
+    {        
+        typedef itk::ImageFileReader< ImageType > ReaderType;
+        ReaderType::Pointer reader = ReaderType::New();
+        reader->SetFileName( argv[1] );
+        image = reader->GetOutput();
+
+        typedef itk::ThresholdImageFilter< ImageType > FilterType;
+        FilterType::Pointer filter = FilterType::New();
+        filter->SetInput( image );
+        filter->SetOutsideValue( 0 );
+        filter->ThresholdOutside( 100, 170 );
+        filter->Update();
+ 
+        return EXIT_SUCCESS;
+    }
+
 }
 
